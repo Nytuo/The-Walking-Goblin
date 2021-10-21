9e6533f939ee92843671998eeb5c5c6b4b4e791d
alexandrejbc <63028565+alexandrejbc@users.noreply.github.com>
2021-10-21 14:36:21 +0200

    Merge branch 'master' of https://github.com/Nytuo/Lutin

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ba101397/Documents/GitHub/Lutin
+# Install script for directory: C:/Users/ca101671/Documents/GitHub/Lutin
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ba101397/Documents/GitHub/Lutin/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ca101671/Documents/GitHub/Lutin/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
