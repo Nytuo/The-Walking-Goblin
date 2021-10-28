e93674599b7e6188fe61c1d71ad425a021f21941
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2021-10-28 14:01:46 +0200

    cmakelist1

--- project/entetes.c
+++ project/entetes.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include "entetes.h"
+
+void
+recuperationGrille(FILE *f, int (*grille)[10], int *largeurGrille, int *nbMonstres, monstre *tabMonstres, int *lLutin,
+                   int *cLutin, int *lPorte, int *cPorte) {
+
+}
