f77a064c0e2a765f7e7a6d10ce6fd1e6a026c16b
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2021-10-29 15:06:12 +0200

    removing cmakebuildfromGit

--- project/TAD.c
+++ project/TAD.c
@@ -2,3 +2,12 @@
 // Created by ca101671 on 21/10/2021.
 //
 
+#include <include/entetes.h>
+
+int deplacementsMonstresLutins (int reponse, int *nbTours, int grille [10][10], int largeurGrille, int nbMonstres,
+                                monstre tabMonstres [], int *lLutin, int *cLutin, int lPorte, int cPorte){
+//reponse = si on gange ou perd la partie
+//tabMonstres = position des montres sur la gerile
+//llutin/clutin/loporte/cporte = respectivemnet la ligne et colonne du lutin et de la porte
+printf("%d %d",lLutin,lPorte);
+}
