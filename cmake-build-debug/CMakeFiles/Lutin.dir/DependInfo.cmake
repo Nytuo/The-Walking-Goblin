dddb2f53557e42046f776e7b975e63a98d43d084
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2021-10-29 15:02:10 +0200

    Revert "Delete cmake-build-debug directory"
    This reverts commit 6dfbfb84abe71013f9ff85d220ae9d3e3a9879c9.

---
+++ cmake-build-debug/CMakeFiles/Lutin.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/ba101397/Documents/GitHub/Lutin/project/TAD.c" "C:/Users/ba101397/Documents/GitHub/Lutin/cmake-build-debug/CMakeFiles/Lutin.dir/project/TAD.c.obj"
+  "C:/Users/ba101397/Documents/GitHub/Lutin/project/fonctions.c" "C:/Users/ba101397/Documents/GitHub/Lutin/cmake-build-debug/CMakeFiles/Lutin.dir/project/fonctions.c.obj"
+  "C:/Users/ba101397/Documents/GitHub/Lutin/project/gestionGrille.c" "C:/Users/ba101397/Documents/GitHub/Lutin/cmake-build-debug/CMakeFiles/Lutin.dir/project/gestionGrille.c.obj"
+  "C:/Users/ba101397/Documents/GitHub/Lutin/project/main.c" "C:/Users/ba101397/Documents/GitHub/Lutin/cmake-build-debug/CMakeFiles/Lutin.dir/project/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../project"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lutin.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lutin.dir/project/TAD.c.obj"
+  "CMakeFiles/Lutin.dir/project/fonctions.c.obj"
+  "CMakeFiles/Lutin.dir/project/gestionGrille.c.obj"
+  "CMakeFiles/Lutin.dir/project/main.c.obj"
+  "Lutin.exe"
+  "Lutin.exe.manifest"
+  "Lutin.pdb"
+  "libLutin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lutin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
