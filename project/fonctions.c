104f5e5ae6b378826e24102881b8c3d4459129ea
MyLifeIsDamba <92926995+MyLifeIsDamba@users.noreply.github.com>
2021-11-12 10:51:55 +0100

    Update fonctions.c

--- project/fonctions.c
+++ project/fonctions.c
@@ -2,5 +2,6 @@
 
 int deplacementsMonstresLutins (int reponse, int *nbTours, int grille [10][10], int largeurGrille, int nbMonstres,
                                 monstre tabMonstres [], int *lLutin, int *cLutin, int lPorte, int cPorte) {
+    return 0;
 
 }
